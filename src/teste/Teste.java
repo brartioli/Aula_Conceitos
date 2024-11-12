e3e6faff2a55e6d10bb502528aab21c3e556a5dd
brartioli <brunaartioli.ba@gmail.com>
2024-11-12 10:21:59 -0300

    testando o teste

---
+++ src/teste/Teste.java
@@ -0,0 +1,23 @@
+package teste;
+
+public class Teste {
+	
+	private String teste;
+
+	public Teste(String teste) {
+		this.teste = teste;
+	}
+
+	public String getTeste() {
+		return teste;
+	}
+
+	public void setTeste(String teste) {
+		this.teste = teste;
+	}
+	
+	
+	
+	
+
+}
